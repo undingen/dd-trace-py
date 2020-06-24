36fe02ca85c8bd00f7026a35ec3c2d0bc9032a1e
Tahir H. Butt <tahir.butt@datadoghq.com>
2020-06-24 08:14:19 -0400

    disable by default

--- ddtrace/contrib/httplib/patch.py
+++ ddtrace/contrib/httplib/patch.py
@@ -19,7 +19,7 @@ log = get_logger(__name__)
 
 
 config._add('httplib', {
-    'distributed_tracing': asbool(get_env('httplib', 'distributed_tracing', default=True)),
+    'distributed_tracing': asbool(get_env('httplib', 'distributed_tracing', default=False)),
 })
 
 
